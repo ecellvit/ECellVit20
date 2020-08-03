03020ebe56149a7828704645eb8dba1450672023
Vinamra Khoria <vinamrakhoria@gmail.com>
2020-08-03 11:09:47 +0530

    Add files via upload

---
+++ src/Sections/Activities.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import DarkBg from '../Components/DarkBg';
+import './Activities.css'
+import Events from '../Components/Events';
+
+function Activities() {
+    const events="Our Events";
+    return (
+        <span id="activities">
+        	<DarkBg head="Activities" content={events}/>
+            <Events/>
+        </span>
+    );
+}
+
+export default Activities;
