0520d58af736abc5da2f59666dfa8287547496e1
gvinayakgupta <guptavinayakg@gmail.com>
2020-08-26 12:52:20 +0530

    Removed social animation

--- src/Components/Social.js
+++ src/Components/Social.js
@@ -1,14 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import './Social.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 
 function Social(props) {
-    useEffect(() => {
-        AOS.init();
-    })
     return (
-        <div className={`${props.position}`} data-aos="zoom-in" data-aos-offset="20">
+        <div className={`${props.position}`}>
             <a href="https://twitter.com/ecell_vit"><span><i className="fab fa-twitter"></i></span></a>
             <a href="https://www.facebook.com/ecellvit"><span><i className="fab fa-facebook-f"></i></span></a>
             <a href="https://www.linkedin.com/company/ecellvitvellore"><span><i className="fab fa-linkedin"></i></span></a>
