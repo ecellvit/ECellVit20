71eb09462375ad89c167301958cc9ab4cc5dad60
Vinayak Gupta <guptavinayakg@gmail.com>
2022-04-07 23:19:45 +0530

    target blank

--- src/Components/Social.js
+++ src/Components/Social.js
@@ -3,11 +3,11 @@ import React from "react";
 function Social(props) {
     return (
         <div className={`${props.position}`}>
-            <a href="https://twitter.com/ecell_vit"><span><i className="fab fa-twitter"></i></span></a>
-            <a href="https://www.facebook.com/ecellvit"><span><i className="fab fa-facebook-f"></i></span></a>
-            <a href="https://www.linkedin.com/company/ecellvitvellore"><span><i className="fab fa-linkedin"></i></span></a>
-            <a href="https://medium.com/e-cell-vit"><span><i className="fab fa-medium"></i></span></a>
-            <a href="https://www.instagram.com/ecell_vit"><span><i className="fab fa-instagram"></i></span></a>
+            <a target="_blank" rel="noreferrer" href="https://twitter.com/ecell_vit"><span><i className="fab fa-twitter"></i></span></a>
+            <a target="_blank" rel="noreferrer" href="https://www.facebook.com/ecellvit"><span><i className="fab fa-facebook-f"></i></span></a>
+            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/company/ecellvitvellore"><span><i className="fab fa-linkedin"></i></span></a>
+            <a target="_blank" rel="noreferrer" href="https://medium.com/e-cell-vit"><span><i className="fab fa-medium"></i></span></a>
+            <a target="_blank" rel="noreferrer" href="https://www.instagram.com/ecell_vit"><span><i className="fab fa-instagram"></i></span></a>
         </div>
     );
 }
