c0b5d9d1eebade0001a7f5880961d81e0b737ba8
PalanikannanM <73993394+Palanikannan1437@users.noreply.github.com>
2022-07-07 18:44:19 +0530

    Update Footer.js

--- src/Sections/Footer.js
+++ src/Sections/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
             <img src="./assets/Logos/ecell-white.png" alt="E-cell VIT" />
             <span className="iia">IDEATE. INNOVATE. ACTUATE.</span>
             <a href="mailto:helloecellvit@gmail.com">helloecellvit@gmail.com</a>
-            <a href="tel:+918293622180">+918293622180</a>
+            <a href="tel:+917259973282">+917259973282</a>
             <Social position="bottom" />
         </div>
     );
